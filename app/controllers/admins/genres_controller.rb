5db4fd88f14981bea1fd5547a041a9a6cfa04ee2
ryota35 <ryota35.1025@gmail.com>
2021-03-17 17:20:02 +0000

    ジャンルの新規登録

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,15 +1,28 @@
 class Admins::GenresController < ApplicationController
 
   def index
+    @genres = Genre.all
+
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to request.referer
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+  end
+
+private
+  def genre_params
+    params[:genre].permit(:name)
   end
 
 end
