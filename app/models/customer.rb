045671b4018d6b033f6bb35100251b5c6dae2840
naoki5454 <naoking250912@icloud.com>
2021-03-18 11:09:03 +0000

    バリデーション追加とプレースフォルダー追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,13 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+         validates :last_name, presence: true
+         validates :first_name, presence: true
+         validates :k_last_name, presence: true
+         validates :k_first_name, presence: true
+         validates :postal_code, presence: true
+         validates :address, presence: true
+         validates :telephone_number, presence: true
+         validates :email, presence: true
 end
