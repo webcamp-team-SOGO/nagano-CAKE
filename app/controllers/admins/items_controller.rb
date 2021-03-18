2d513909fed75e011d1c3cfcc1a7115c46fa8093
KAIGOTO <ec2-user@ip-172-31-72-211.ec2.internal>
2021-03-18 03:20:31 +0000

    一覧画面作成

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -1,15 +1,21 @@
 class Admins::ItemsController < ApplicationController
 
   def new
+    @item = Item.new
   end
 
   def index
+    @items = Item.page(params[:page]).reverse_order
+    @item = Item.new
   end
 
   def show
   end
 
   def create
+    @item = Item.new(params[:id])
+    @item.save
+    redirect_to admin_item_path(@item)
   end
 
   def edit
