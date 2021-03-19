a17a2b4cb449421cc0cebe03280ef31b2f8c1eeb
ryota35 <ryota35.1025@gmail.com>
2021-03-19 09:06:05 +0000

    会員一覧作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,37 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Customer.create(
-   email: 'yes@no.com',
-   last_name: 'yes',
-   first_name: 'no',
-   k_last_name: 'yes',
-   k_first_name: 'no',
-   postal_code: '1600022',
-   address: '東京都新宿区新宿２丁目５−１０ 成信ビル 4階',
-   telephone_number: '09012345678',
-   password: 'password',
-)
+
+
+   7.times do |n|
+    Customer.create!(
+      email: "edogawa#{n + 1}@test.com",
+      last_name: "江戸川#{n + 1}",
+      first_name: "コナン#{n + 1}",
+      k_last_name: "エドガワ#{n + 1}",
+      k_first_name: "コナン#{n + 1}",
+      postal_code: '1600022',
+      address: "東京都新宿区新宿２丁目５−１０ 成信ビル 4階#{n + 1}",
+      telephone_number: "09012345678#{n + 1}",
+      password: "password#{n + 1}",
+      is_valid: "true"
+    )
+  end
+
+   7.times do |n|
+    Customer.create!(
+      email: "mouriran#{n + 1}@test.com",
+      last_name: "江戸川#{n + 1}",
+      first_name: "コナン#{n + 1}",
+      k_last_name: "エドガワ#{n + 1}",
+      k_first_name: "コナン#{n + 1}",
+      postal_code: '1600022',
+      address: "東京都新宿区新宿２丁目５−１０ 成信ビル 4階#{n + 1}",
+      telephone_number: "08000000000#{n + 1}",
+      password: "password#{n + 1}",
+      is_valid: "false"
+    )
+  end
 
 Admin.create!(
    email: 'test@test.com',
