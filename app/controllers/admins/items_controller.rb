96b4eb9f68270c56d0ab19883a3d99d00a7b2f0d
KAIGOTO <k.510.ex@icloud.com>
2021-03-19 02:45:17 +0000

    指摘の部分修正（ま商品一覧は反映されていません）

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -2,26 +2,35 @@ class Admins::ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    @items = Item.all
+    # @genres = Genre.all
   end
 
   def index
-    @items = Item.page(params[:page]).reverse_order
+    @items = Item.all
     @item = Item.new
+    @item_k = Item.page(params[:page]).per(10)
   end
 
   def show
   end
 
   def create
-    @item = Item.new(params[:id])
+    @item = Item.new(item_params)
     @item.save
-    redirect_to admin_item_path(@item)
+    redirect_to admins_item_path(@item)
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :taxfree, :is_valid)
+    #後ほどジャンル追加する
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  belongs_to :admin
+  
+  attachment :image
+  validates :name, presence: true ,length: {minimum: 2,maximum: 30}
+  validates :introduction, presence: true, length: {maximum: 200}
+#   validates :genre, inclusion: { in: %w(draft publish private) }
+  validates :taxfree, presence: true
+  validates :is_valid, inclusion: { in: [true, false] }
 end
