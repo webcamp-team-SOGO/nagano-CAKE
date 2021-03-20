a24747136459528d4a4122fc3e5f22b19687bfbb
Ryo-2525 <seama.24.music@gmail.com>
2021-03-20 03:01:33 +0000

    カート作成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,7 @@ class HomesController < ApplicationController
 
   def top
     @items = Item.limit(4).order(" created_at DESC ")
-    #@customer = current_customer
+    
   end
 
   def about
--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,14 +1,14 @@
 class Public::CartItemsController < ApplicationController
 
   def index
-    #@cart_items = current_customer.cart_items
+    @cart_items = current_customer.cart_items
   end
 
   def create
     if @cart_item.blank?
-      @cart_item = current_cart.cart_items.build(item_id: params[:item_id])
+      @cart_item = current_customer.cart_items.build(item_id: params[:item_id])
     end
-    @cart_item.quantity += params[:number].to_i
+    @cart_item.number += params[:number].to_i
     @cart_item.save
     redirect_to cart_items_path
   end
