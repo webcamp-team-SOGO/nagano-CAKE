232152757990d97e03ce63794b2812452bbb90b7
naoki5454 <naoking250912@icloud.com>
2021-03-20 03:06:15 +0000

    一応完成、登録された時に確認

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -10,9 +10,16 @@ class Public::DeliveriesController < ApplicationController
   end
 
   def edit
+    @delivery = Delivery.find(params[:id])
   end
 
   def update
+    @delivery = Delivery.find(params[:id])
+    if @delivery.update(delivery_params)
+      redirect_to deliveries_path
+    else
+      render :edit
+    end
   end
 
 end
