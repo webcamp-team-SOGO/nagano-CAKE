75a31d580f60b681fe60f87dd1a3165056101d9c
naoki5454 <naoking250912@icloud.com>
2021-03-21 12:28:57 +0000

    #外した

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,14 +4,14 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         #validates :last_name, presence: true
-         #validates :first_name, presence: true
-         #validates :k_last_name, presence: true
-         #validates :k_first_name, presence: true
-         #validates :postal_code, presence: true
-         #validates :address, presence: true
-         #validates :telephone_number, presence: true, uniqueness: true
-         #validates :email, presence: true, uniqueness: true
+         validates :last_name, presence: true
+         validates :first_name, presence: true
+         validates :k_last_name, presence: true
+         validates :k_first_name, presence: true
+         validates :postal_code, presence: true
+         validates :address, presence: true
+         validates :telephone_number, presence: true, uniqueness: true
+         validates :email, presence: true, uniqueness: true
 
 
   has_many :cart_items
