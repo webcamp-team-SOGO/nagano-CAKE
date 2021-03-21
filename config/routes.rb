6e0b6c8fac9517d4f996667019c8438ffe3cff91
Ryo-2525 <seama.24.music@gmail.com>
2021-03-21 09:12:07 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -31,15 +31,11 @@ Rails.application.routes.draw do
     resources :cart_items, only: [:index, :create, :update, :destroy]
     resources :orders, only: [:new, :index, :create, :show]
     resources :deliveries, only: [:index, :create, :update, :destroy, :edit]
+    get 'orders/thanks' => 'public/orders#thanks'
     get 'confirm/:id' => 'customers#confirm', as: 'confirm_confirm'
     patch 'withdraw/:id' => 'customers#withdraw', as: 'withdraw_customer'
   end
 
   get 'customers/cart_items/destroy_all' => 'public/cart_items#destroy_all'
-  
-  resources :orders, only: [:new, :create, :index, :show] do
-      collection do
-        post "confirm" 
-      end
-    end
+        
 end
