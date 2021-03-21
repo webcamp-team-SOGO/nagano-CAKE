d98fb646c745758339fee1af734adfc979483e6d
naoki5454 <naoking250912@icloud.com>
2021-03-21 02:28:32 +0000

    save

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,6 +4,9 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    
+    @cart_items = Cart_item.all
+    @taxfree = (@item.taxfree * 1.1).round
   end
 
   def create
