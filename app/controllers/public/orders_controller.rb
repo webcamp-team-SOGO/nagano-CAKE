fe895aeb0dfe1b177d00d93c44809e9575394e51
ryota35 <ryota35.1025@gmail.com>
2021-03-23 07:09:22 +0000

    recent

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -13,11 +13,22 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    @order = Order.find_or_initialize_by(id: params[:id])
-    @order.assign_attributes(order_params)
-    # @order_item = OrderItem.find_or_initialize_by(id: params[:id])
+    @order = Order.new(customer_id: current_customer, payment_method: params[:order][:payment_method])
+
+    if params[:order][:address_option] == "0"
+      @order.address = current_customer.address
+      @order.postal_code = current_customer.postal_code
+      @order.name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:address_option] == "1"
+      @address = Delivery.find(params[:order][:delivery_id])
+      @order.address = @address.address
+      @order.postal_code = @address.postal_code
+      @order.name = @address.name
+    elsif params[:order][:address_option] == "2"
+      @order = Order.new(order_params)
+    end
+
     @order_item = OrderItem.new
-    # @order_item.assign_attributes(order_item_params)
     @customer = current_customer
     @cart_items = @customer.cart_items
     @total_payment = (@cart_items.to_a.sum{|x| x.item.taxfree * x.number} * 1.1).floor.to_s(:delimited)
@@ -26,8 +37,16 @@ class Public::OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     @order.save
-    @order_item = OrderItem.new(order_item_params)
-    @order_item.save
+    @cart_items = @customer.cart_items
+    @cart_items.each do |cart_item|
+      OrderItem.create(
+        order_id: @order.id,
+        item_id: cart_item.item.id,
+        price: cart_item.item.taxfree * 1.1,
+        number: cart_item.number
+        )
+    end
+
     redirect_to order_thanks_path
   end
 
@@ -46,7 +65,7 @@ class Public::OrdersController < ApplicationController
   end
 
   def order_item_params
-    params.permit(:number, :price )
+    params.permit(:number, :price)
   end
 
 end
--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,3 +1,9 @@
 class Delivery < ApplicationRecord
   belongs_to :customer
+
+  def deliveries
+    self.postal_code + self.address + self.name
+  end
+
+
 end
