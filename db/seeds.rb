b9ce279a4058e992f70ec87ed0d5d2d36d23d82f
ryota35 <ryota35.1025@gmail.com>
2021-03-24 09:21:08 +0000

    諸々修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,3 +46,31 @@ Admin.create(
    is_valid: "false"
  )
  end
+ 
+ 6.times do |n|
+ Genre.create!(
+   name: "焼き菓子#{n + 1}",
+)
+end
+
+6.times do |n|
+ Item.create!(
+   genre_id: "#{n + 1}",
+   name: "お菓子#{n + 1}",
+   image: File.open("./app/assets/images/img/test#{n + 1}.jpg"),
+   introduction: "おいしいよ！最高だよ！！",
+   taxfree: "#{(n + 1) * 500}",
+   is_valid: 'true',
+)
+end
+
+6.times do |n|
+ Item.create!(
+   genre_id: "#{n + 1}",
+   name: "お菓子#{n + 7}",
+   image: File.open("./app/assets/images/img/test#{n + 7}.jpg"),
+   introduction: "おいしいよ！最高だよ！！",
+   taxfree: "#{(n + 1) * 1000}",
+   is_valid: 'false',
+)
+end
