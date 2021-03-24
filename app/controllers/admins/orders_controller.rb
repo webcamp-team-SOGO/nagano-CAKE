9c4cf0bac9523af17aa69115f53f0fb3e7959b08
naoki5454 <naoking250912@icloud.com>
2021-03-24 03:41:12 +0000

    注文履歴詳細のshowアクションの変更

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -2,30 +2,29 @@ class Admins::OrdersController < ApplicationController
 before_action :authenticate_admin!
 
   def index
-
     case params[:order_sort]
 
     when "0"
      @orders = Order.page(params[:page]).per(10)
     else
      @customer = Customer.find(params[:id])
-     @orders = @customer.orders.page(params[:page]).per(10)
+     @orders = Customer.order.page(params[:page]).per(10)
     end
 
   end
 
   def show
     @order = Order.find(params[:id])
-    @order_items = OrderItem.all
+    @order_items = @order.order_items
   end
 
   def update
-    @order = Order.find(params[:id]) 
+    @order = Order.find(params[:id])
     @order.update(order_params)
     # flash[:success] = "更新に成功しました"
     redirect_to admins_order_path
   end
-  
+
   private
   def order_params
   	params.require(:order).permit(:status)
